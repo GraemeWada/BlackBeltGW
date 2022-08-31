e3c845ef65bffd9e0bff2825eedd37f4a46451a4
cn_unity1 <cnmarda.unity@gmail.com>
2022-08-31 17:56:40 -0600

    Fixed movement calculation problem and started velocity as a vector magnitude

--- Assets/Assets/Scripts/Controls.cs
+++ Assets/Assets/Scripts/Controls.cs
@@ -16,43 +16,49 @@ public class Controls : MonoBehaviour
 
     public GravitySwitch g;
     public int temp;
+
     // Start is called before the first frame update
     void Start()
     {
         Physics2D.gravity = new Vector2(0, -9.81f);
-        if (g == null) 
+        if (g != null) 
         {
-            RightV.x = Mathf.Cos(g.dir[g.GCCounter]);
-            RightV.y = Mathf.Sin(g.dir[g.GCCounter]);
-            LeftV.x = Mathf.Cos(g.dir[g.GCCounter]+180);
-            LeftV.y = Mathf.Sin(g.dir[g.GCCounter]+180);
+            int dir = g.dir[g.GCCounter];
+            RightV.x = Mathf.Cos(Mathf.Deg2Rad*dir);
+                RightV.y = Mathf.Sin(Mathf.Deg2Rad*dir);
+                LeftV.x = Mathf.Cos(Mathf.Deg2Rad*(dir+180));
+                LeftV.y = Mathf.Sin(Mathf.Deg2Rad*(dir+180));
             temp = g.GCCounter;
         }
         else
         {
             RightV.x = Mathf.Cos(0);
             RightV.y = Mathf.Sin(0);
-            LeftV.x = Mathf.Cos(180);
-            LeftV.y = Mathf.Sin(180);
+            LeftV.x = Mathf.Cos(Mathf.PI);
+            LeftV.y = Mathf.Sin(Mathf.PI);
         }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        float v = Mathf.Sqrt(Mathf.Pow(Mathf.Abs(rb.velocity.x), 2)+Mathf.Pow(Mathf.Abs(rb.velocity.y), 2));
+        //float h = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 
         if( Input.GetAxis("Horizontal") < 0 && rb.velocity.x > (speedcap * -1))
         {
             //Vector3 v = new Vector3(-1 * speed * -Input.GetAxis("Horizontal"), 0, 0);
             rb.AddForce(LeftV * speed * 1.5f, ForceMode2D.Impulse);
-            Debug.Log(rb.velocity);
+            //Debug.Log(rb.velocity);
+            //Left
         }
         if (Input.GetAxis("Horizontal") > 0 && rb.velocity.x < speedcap)
         {
             //Vector3 v = Vector3.right * (float)(speed/1.5) * Input.GetAxis("Horizontal");
             rb.AddForce(RightV * speed, ForceMode2D.Impulse);
-            Debug.Log(rb.velocity);
+            //Debug.Log(rb.velocity);
+            //Right
         }
 
         //rb.AddForce(new Vector3(x, 0, 0), ForceMode2D.Impulse);
@@ -60,13 +66,16 @@ public class Controls : MonoBehaviour
 
     void Update()
     {
-        if(g != null || g.GCCounter != temp)
+        if(g != null)
         {
-            RightV.x = Mathf.Cos(g.dir[g.GCCounter]);
-            RightV.y = Mathf.Sin(g.dir[g.GCCounter]);
-            LeftV.x = Mathf.Cos(g.dir[g.GCCounter] + 180);
-            LeftV.y = Mathf.Sin(g.dir[g.GCCounter] + 180);
-            temp = g.GCCounter;
+            if(g.GCCounter != temp){
+                int dir = g.dir[g.GCCounter];
+                RightV.x = Mathf.Cos(Mathf.Deg2Rad*dir);
+                RightV.y = Mathf.Sin(Mathf.Deg2Rad*dir);
+                LeftV.x = Mathf.Cos(Mathf.Deg2Rad*(dir+180));
+                LeftV.y = Mathf.Sin(Mathf.Deg2Rad*(dir+180));
+                temp = g.GCCounter;
+            }
         }
     }
 
@@ -74,7 +83,7 @@ public class Controls : MonoBehaviour
     {
         if(c.gameObject.tag == "PushZone")
         {
-rb.AddForce(Vector3.up * stuck, ForceMode2D.Impulse);
+            rb.AddForce(Vector3.up * stuck, ForceMode2D.Impulse);
         }
         
     }
